c5532361b3e81a3ee52c877efedcc3e05ac2ba49
Thierry PROST <t.prost@everdreamsoft.com>
2017-05-07 17:21:44 +0200

    Fenêtre globale + event quitter

---
+++ night_mission.py
@@ -0,0 +1,36 @@
+"""
+@ToDo Describe what's missing
+- 1st task ...
+"""
+
+__version__ = "$Id:$"
+__docformat__ = "reStructuredText"
+
+# -*- coding: utf-8 -*-
+import sys, random, pygame
+from pygame.locals import *
+from pygame.color import *
+
+import pymunk
+from pymunk import Vec2d
+import pymunk.pygame_util
+
+pygame.init()
+
+def main():
+    # Init vars
+    running = True
+    clock = pygame.time.Clock()
+    size = width, height = 1024, 768
+    screen = pygame.display.set_mode(size)
+
+# Main Loop
+    while running:
+        # Events loop
+        for event in pygame.event.get():
+            # Manage quit / closing window event
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+if __name__ == '__main__': main()
