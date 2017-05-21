7f93061f31114c9b74ed3af6c98714440ba29515
Thierry PROST <t.prost@everdreamsoft.com>
2017-05-21 11:35:12 +0200

    Draw physics static lines all around the game area

--- night_mission.py
+++ night_mission.py
@@ -32,7 +32,7 @@ def main():
     space.gravity = (0.0, -900.0)
     draw_options = pymunk.pygame_util.DrawOptions(surface)
 
-    fp = [(20,-20), (-120, 0), (20,20)]
+    fp = [(12,-12), (-80, 0), (12,12)]
     mass = 100
     moment = pymunk.moment_for_poly(mass, fp)
 
@@ -40,16 +40,22 @@ def main():
     balls = []
 
     # Some Walls
-    #static_lines = [pymunk.Segment(space.static_body, (150, 100.0), (50.0, 550.0), 1.0)
-    #                ,pymunk.Segment(space.static_body, (450.0, 100.0), (550.0, 550.0), 1.0)
-    #                ,pymunk.Segment(space.static_body, (50.0, 550.0), (300.0, 600.0), 1.0)
-    #                ,pymunk.Segment(space.static_body, (300.0, 600.0), (550.0, 550.0), 1.0)
-    #                ,pymunk.Segment(space.static_body, (300.0, 420.0), (400.0, 400.0), 1.0)]
+    static_lines = [pymunk.Segment(space.static_body, (250, 0), (250, 383), 1.0),
+                    pymunk.Segment(space.static_body, (250, 383), (265, 390), 1.0),
+                    pymunk.Segment(space.static_body, (265, 390), (265, 718), 1.0),
+                    pymunk.Segment(space.static_body, (265, 718), (280, 738), 1.0),
+                    pymunk.Segment(space.static_body, (280, 738), (370, 738), 1.0),
+                    pymunk.Segment(space.static_body, (370, 738), (415, 765), 1.0),
+                    pymunk.Segment(space.static_body, (415, 765), (655, 765), 1.0),
+                    pymunk.Segment(space.static_body, (655, 765), (700, 753), 1.0),
+                    pymunk.Segment(space.static_body, (700, 753), (732, 713), 1.0),
+                    pymunk.Segment(space.static_body, (732, 713), (742, 648), 1.0),
+                    pymunk.Segment(space.static_body, (742, 648), (742, 0), 1.0)]
 
-    #for line in static_lines:
-    #    line.elasticity = 0.7
-    #    line.group = 1
-    #space.add(static_lines)
+    for line in static_lines:
+        line.elasticity = 0.7
+        line.group = 1
+    space.add(static_lines)
 
     # "bumpers"
     #for p in [(240,500), (360,500)]:
@@ -61,8 +67,8 @@ def main():
 
     # Right flipper
     r_flipper_body = pymunk.Body(mass, moment)
-    r_flipper_body.position = 650, 90
-    #r_flipper_shape = pymunk.Poly(r_flipper_body, fp)
+    r_flipper_body.position = 600, 87
+    r_flipper_shape = pymunk.Poly(r_flipper_body, fp)
     space.add(r_flipper_body, r_flipper_shape)
 
     r_flipper_joint_body = pymunk.Body(body_type=pymunk.Body.KINEMATIC)
@@ -73,7 +79,7 @@ def main():
 
     # Left flipper
     l_flipper_body = pymunk.Body(mass, moment)
-    l_flipper_body.position = 350, 90
+    l_flipper_body.position = 380, 87
     l_flipper_shape = pymunk.Poly(l_flipper_body, [(-x,y) for x,y in fp])
     space.add(l_flipper_body, l_flipper_shape)
 
@@ -92,6 +98,10 @@ def main():
         surface.fill((0, 0, 0))
         drawStuff(surface)
 
+        # Define mouse position
+        mouseX = pygame.mouse.get_pos()[1]
+        mouseY = pygame.mouse.get_pos()[0]
+
         # Events loop
         for event in pygame.event.get():
             # Manage quit / closing window event
@@ -103,12 +113,12 @@ def main():
                     pygame.quit()
                     sys.exit()
                 elif event.key == K_j:
-                    r_flipper_body.apply_impulse_at_local_point(Vec2d.unit() * 40000, (-100,0))
+                    r_flipper_body.apply_impulse_at_local_point(Vec2d.unit() * 40000, (-80,0))
                 elif event.key == K_f:
-                    l_flipper_body.apply_impulse_at_local_point(Vec2d.unit() * -40000, (-100,0))
+                    l_flipper_body.apply_impulse_at_local_point(Vec2d.unit() * -40000, (-80,0))
                 elif event.type == KEYDOWN and event.key == K_b:
                     mass = 1
-                    radius = 25
+                    radius = 15
                     inertia = pymunk.moment_for_circle(mass, 0, radius, (0,0))
                     body = pymunk.Body(mass, inertia)
                     x = random.randint(315,650)
@@ -121,8 +131,8 @@ def main():
         # Display Rasta Rockets
         space.debug_draw(draw_options)
 
-        r_flipper_body.position = 650, 90
-        l_flipper_body.position = 350, 90
+        r_flipper_body.position = 600, 87
+        l_flipper_body.position = 380, 87
         r_flipper_body.velocity = l_flipper_body.velocity = 0,0
 
         # Remove any balls outside
@@ -143,6 +153,6 @@ def main():
         clock.tick(50)
         screen.blit(surface, (0, 0))
         pygame.display.flip()
-        pygame.display.set_caption("Night Mission - PinBall - " + "FPS: " + str(clock.get_fps()))
+        pygame.display.set_caption("Night Mission - PinBall - " + "FPS: " + str(clock.get_fps()) + " - Mouse Position : " + "mouseX: {0} - mouseY: {1}".format(mouseX, mouseY))
 
 if __name__ == '__main__': main()
